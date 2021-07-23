935dd7a32803e39f8b1d70661dd5e0449eea70d3
dev-vicki <vignesh.r1999@gmail.com>
2021-07-23 15:47:43 +0530

    userName and Score Initialized

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // importing libraries from npm
 const readLineSync = require("readline-sync");
 const chalk = require("chalk");
+
+// Taking userName
+const userName = readLineSync.question(chalk.yellow.bold("To play quiz please enter your name : "));
+
+// score initialized
+let score = 0;
